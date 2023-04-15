10bf524238765d6e0925c83807d5e0e59155d369
bena24 <24benanderson@gmail.com>
2023-04-14 20:13:29 -0700

    Simulation update
    Added new functions and graphing capability.
    Added relationship of power ratio and yaw rate.

--- simulation/simulation.py
+++ simulation/simulation.py
@@ -1,5 +1,53 @@
 import math
 import random
+import array as arr
+from graph import display_graphs
+
+# Used Terms:
+"""
+Yaw Rate: rad/s
+Longitudinal Velocity: Vx, m/s
+Cornering stiffness at rear wheel: Cy_r, N/rad
+Cornering stiffness at front wheel: Cy_f, N/rad
+Inertia Moment: Izz, Kg/m^2
+Mass: m, Kg
+Front wheelbase: lf, m
+Rear wheelbase: lr, m
+Steering Angle, St_a, degrees (Convert from radians)
+Yaw Moment: M_V, Nm
+Yaw Moment around vertical axis: Mz
+Lateral Velocity: Vy, m/s
+Gear Ratio: Gr, -
+Half the track of the car: Tr, m
+Radius of the wheel: Rw, m (0.2286)
+"""
+
+# radians/s (wheel speed)
+wheelAngularVelocity = 0 
+
+# radians/s^2 (wheel torque basically)
+wheelAngularAccel = 28.2744 
+
+# max angular acceleration in radians/s^2
+maxAngularAccel = 1 
+
+# Wheel radius in meters
+wheelRadius = 0.4
+
+#Slippage in percentage (1.0 as 0%)
+slippage = 1.0
+
+# Gear ratio
+Gr = 4.1
+
+# Inertia Moment around z axis
+Izz = 120
+
+# Half track of rear axle in meters
+Tr = 0.6604 
+
+# Radius of wheel
+Rw = 0.2286
 
 # lf (float): Distance between the center of gravity and the front axle in meters
 lf = 0.885
@@ -18,6 +66,8 @@ Cy_r = 7302
 
 # v_cg (float): Velocity of the center of gravity in m/s
 v_cg = 1
+
+weight = m * 9.81
 
 # PID Controller class for controlling the car's yaw rate
 class PIDController:
@@ -46,6 +96,10 @@ class PIDController:
         output = self.Kp * error + self.Ki * self.integral + self.Kd * derivative  # Calculate control output
         self.previous_error = error  # Store current error as previous error for next iteration
         return output  # Return control output
+    
+def radians_to_ms(radians):
+    ms = radians * wheelRadius
+    return ms
 
 # Function for converting degrees to radians
 def degrees_to_radians(degrees):
@@ -81,7 +135,7 @@ def calculate_desired_yaw_rate(v_cg, delta):
     # Return the calculated desired yaw rate
     return desired_yaw_rate
 
-def calculate_yaw_rate(Cy_f, Cy_r):
+def calculate_yaw_rate(Cy_f, Cy_r, Vx, St_a, Trr, Trl):
     """
     Calculates the yaw rate of a vehicle given the distance between the center
     of gravity and the front and rear axles, the mass of the vehicle, and the
@@ -90,15 +144,71 @@ def calculate_yaw_rate(Cy_f, Cy_r):
     Args:
     Cy_f (float): Cornering stiffness of the front tires in N/rad
     Cy_r (float): Cornering stiffness of the rear tires in N/rad
+    Vx, (float): Current velocity of car in m/s
+    St_a, (float): Current steering angle in radians
+    Trr, (float): Current power from rear right wheel in Nm
+    Trl, (float): Current power from rear left wheel in Nm
 
     Returns:
     float: Yaw rate of the vehicle in radians per second
     """
     # Calculate the yaw rate using the given formula
-    Ku = (lr * m) / (Cy_f * (lf + lr)) - (lf * m) / (Cy_r * (lf + lr))
+    Ku = ((-lf * Cy_f + lr * Cy_r) / (Izz * Vx)) - ((lf**2 * Cy_f + lr**2 * Cy_r) / (Izz * Vx)) + ((lf * Cy_f) / Izz) * St_a + (1 / 0.05 * Izz) * calculate_delta_torque(Trr, Trl)
 
     # Return the calculated yaw rate
     return Ku
+
+def calculate_yaw_moment(Trr, Trl):
+    """
+    Calculates the momentum of how the power ratio of each wheel
+    influences the yaw rate of the car
+
+    Args:
+    Trr, (float): Current power from rear right wheel in Nm
+    Trl, (float): Current power from rear left wheel in Nm
+
+    Returns:
+    float: Yaw moment ratio
+    """
+    # Calculate the yaw moment using the given formula
+    M_V = (Trr - Trl) * Tr
+
+    # Return the calculated yaw moment
+    return M_V
+
+def calculate_lateral_velocity(Vx, St_a):
+    """
+    Calculates the lateral velocity of the car during turns.
+
+    Args:
+    Vx, (float): Current velocity of the car in m/s
+    St_a (float): Steering angle in radians
+
+    Returns:
+    float: Lateral velocity (also known as lateral force)
+    """
+    # Calculate the lateral velocity using the given formula
+    Vy = ((-(Cy_f + Cy_r)/(m * Vx)) + (((-lf * Cy_f + lr * Cy_r)/(m * Vx)) - Vx)) + (Cy_f / (m * Vx)) * St_a
+
+    # Return the calculated lateral velocity
+    return Vy
+
+def calculate_delta_torque(Trr, Trl):
+    """
+    Calculates the difference in torque between each wheel
+
+    Args:
+    Trr, (float): Current power from rear right wheel in Nm
+    Trl, (float): Current power from rear left wheel in Nm
+
+    Returns:
+    float: Difference in torque in Nm
+    """
+    # Calculate the delta torque using the given formula
+    delt_T = (Rw / ((2 * Tr) * Gr)) * calculate_yaw_moment(Trr, Trl)
+
+    #return delt_T
+    return delt_T
 
 # Function for calculating the throttle adjustment required to reach the desired yaw rate
 def calculate_throttle_adjustment(pid_controller, current_yaw_rate, desired_yaw_rate):
@@ -117,32 +227,108 @@ def calculate_throttle_adjustment(pid_controller, current_yaw_rate, desired_yaw_
     throttle_adjustment = pid_controller.control(error)  # Calculate throttle adjustment using PID controller
     return throttle_adjustment  # Return throttle adjustment required to reach the desired yaw rate
 
-def simulate(v_cg, delta_degrees):
+def magic_formula(w, p, b=10, c=1.9, d=1, e=.97):
+    """
+
+    The magic formula [1] computes the longitudinal force between wheel and tarmac
+    in function of the longitudinal tire slip.
+
+    [1] Pacejka, H. B. Tire and Vehicle Dynamics. Elsevier Science, 2005
+
+    Parameters
+    ----------
+    num : int, optional
+        Number of samples to generate. Default is 50. Must be non-negative.
+    p : float
+        Weight applied on the tire [N].
+    b : float
+        Stiffness factor. Default value corresponds to dry tarmac.
+    c : float
+        Shape factor. Default value corresponds to dry tarmac.
+    d : float
+        Peak factor. Default value corresponds to dry tarmac.
+    e : float
+        Curvature factor. Default value corresponds to dry tarmac.
+
+    Returns
+    -------
+    F : float or array
+        If x is a slip ratio, Func is the longitudinal force between tire and tarmac [N], if x is a wheel slip angle, Func
+        is the lateral force.
+    alpha : float, array_like
+        Tire slip ratio or slip angle.
+    """
+    p = p - 1
+    F = w * d * math.sin(c * math.atan(b * p * (1 - e) + e * b * p - math.atan(b * p)))
+    return F
+
+def simulate(v_cg, w_Velocity, rl_torqueWheel, rr_torqueWheel, steering_a):
     """
     Simulate the vehicle dynamics.
 
     Parameters:
-    v_cg (float): The center of gravity velocity.
-    delta_degrees (float): The steering angle in degrees.
+    v_cg (float): The center of gravity velocity in m/s.
+    w_Velocity (float): Total rear longitudinal velocity of wheels in m/s.
+    rl_torqueWheel (float): Rear left wheel torque in Nm.
+    rr_torqueWheel (float): Rear right wheel torque in Nm.
+    steering_a (float): The steering angle in radians.
 
     Returns:
-    tuple: A tuple containing longitudinal acceleration (m/s^2), lateral acceleration (m/s^2), and yaw rate (rad/s).
+    tuple: A tuple containing velocity (m/s), lateral velocity (m/s), wheel velocity (m/s), and yaw rate (radians/s)
     """
-    delta = degrees_to_radians(delta_degrees)
-    yaw_rate = calculate_yaw_rate(Cy_f, Cy_r)
-    ay = v_cg * yaw_rate
-    ax = v_cg ** 2 / (lf + lr) * delta
-    return ax, ay, yaw_rate
+    a_x = arr.array('f', [v_cg])
+    a_y = arr.array('f', [0])
+    wheel_velocity = arr.array('f', [w_Velocity])
+    curr_yaw_rate = arr.array('f', [0])
+    rl_wheelAngularAccel = radians_to_ms((2*rl_torqueWheel) / (9 * wheelRadius))
+    rr_wheelAngularAccel = radians_to_ms((2*rr_torqueWheel) / (9 * wheelRadius))
+    rl_slippage = 1.0
+    rr_slippage = 1.0
+    i = 0
+    while i < 1000:
+            
+        rl_wheelAngularAccel = radians_to_ms((2*rl_torqueWheel) / (9 * wheelRadius))
+        rr_wheelAngularAccel = radians_to_ms((2*rr_torqueWheel) / (9 * wheelRadius))
+        rl_slippage = (v_cg + rl_wheelAngularAccel) / v_cg
+        rr_slippage = (v_cg + rr_wheelAngularAccel) / v_cg
+        if rl_slippage < 1:
+            rl_slippage = 1.0
+        if rr_slippage < 1:
+            rr_slippage = 1.0
+        F = magic_formula(weight, (rl_slippage + rr_slippage) / 2)
+        LongAccel = (F / m) / 10
+        v_cg = v_cg + LongAccel
+        rl_wheelAngularVelocity = rl_slippage * v_cg
+        rr_wheelAngularVelocity = rr_slippage * v_cg
+        a_x.append(a_x[i] + LongAccel)
+        wheel_velocity.append((rl_wheelAngularVelocity + rr_wheelAngularVelocity) / 2)
+        a_y.append(calculate_lateral_velocity(v_cg, steering_a))
+        curr_yaw_rate.append(calculate_yaw_rate(Cy_f, Cy_r, v_cg, steering_a, rr_torqueWheel, rl_torqueWheel))
+        i = i + 1
+
+    return a_x, a_y, wheel_velocity, curr_yaw_rate
 
 if __name__ == '__main__':
-    # Generate random steering angle and velocity for simulation
-    random_steering_angle = random.uniform(-230, 230)
-    random_velocity = random.uniform(0, 50)
+    
+    # Simulate vehicle dynamics with given velocities, torque applied to each wheel, and steering wheel angle
+    ax, ay, wheel_velocity, yaw_rate = simulate(1, 1, 16, 16, 0)
 
-    # Simulate vehicle dynamics with given steering angle and velocity
-    ax, ay, yaw_rate = simulate(random_velocity, random_steering_angle)
-    desired_yaw_rate = calculate_desired_yaw_rate(random_velocity, random_steering_angle)
+    # Desired yaw rate will be compared to resulting yaw rate later...
+    # desired_yaw_rate = calculate_desired_yaw_rate(random_velocity, random_steering_angle)
 
+    # Calculate time
+    i = 1
+    time = arr.array('f', [0])
+    total_slip = arr.array('f', [((wheel_velocity[i] / ax[i]) - 1) * 100])
+    while i < len(ax):
+        time.append(i / 10)
+        total_slip.append(((wheel_velocity[i] / ax[i]) - 1) * 100)
+        i += 1
+    
+    # Display results
+    display_graphs(time, ax, ay, wheel_velocity, total_slip, yaw_rate)
+    
+    """
     # Print the simulation results
     print(f"Random steering angle (degrees): {random_steering_angle}")
     print(f"Random velocity (m/s): {random_velocity}")
@@ -175,3 +361,4 @@ if __name__ == '__main__':
     # Print the final throttle percentages for left and right wheels
     print(f"Left wheel throttle percentage: {left_wheel_throttle}")
     print(f"Right wheel throttle percentage: {right_wheel_throttle}")
+    """

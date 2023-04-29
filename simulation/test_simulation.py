8d2f59e7f017a745d05e3edce8bcfe074ce1a4fa
Matt Moradi <matthew.moradi@pdx.edu>
2023-04-28 23:13:47 -0700

    Fixed Existing Sim Tests

--- simulation/test_simulation.py
+++ simulation/test_simulation.py
@@ -2,6 +2,10 @@ from simulation import *
 import pytest
 
 random_steering_angle = random.uniform(-230, 230)
+Vx = v_cg
+St_a = 0.4 # last simulate argument (steering_a)
+Trr = 5 # 4th simulate argument (rr_torqueWheel)
+Trl = 1 # 3rd simulate argument (rl_torqueWheel)
 
 def test_degrees_to_radians():
     degrees = random.uniform(0, 360)
@@ -11,25 +15,10 @@ def test_degrees_to_radians():
 
 def test_calculate_desired_yaw_rate():
     delta = random.uniform(-230, 230)
-    Ku = calculate_yaw_rate(Cy_f, Cy_r)
-    assert calculate_desired_yaw_rate(v_cg, delta) == v_cg / ((lf + lr) + Ku * v_cg**2) * delta
+    desired_yaw_rate = (v_cg / (lf + lr) - 0.00168 * math.pow(v_cg, 2)) * delta
+    assert calculate_desired_yaw_rate(v_cg, delta) == desired_yaw_rate
 
 def test_calculate_yaw_rate():
-    assert calculate_yaw_rate(Cy_f, Cy_r) == (lr * m) / (Cy_f * (lf + lr)) - (lf * m) / (Cy_r * (lf + lr))
-
-def test_calculate_throttle_adjustment():
-    random_velocity = random.uniform(0, 50)
-    desired_yaw_rate = calculate_desired_yaw_rate(random_velocity, random_steering_angle)
-    current_yaw_rate = simulate(random_velocity, random_steering_angle)[2]
-    pid_controller = PIDController(2, 0.3, 20, 0.2)
-
-    error = desired_yaw_rate - current_yaw_rate
-    throttle_adjustment = pid_controller.control(error)
-    assert calculate_throttle_adjustment(PIDController(2, 0.3, 20, 0.2), current_yaw_rate, desired_yaw_rate) == throttle_adjustment
-
-def test_simulate():
-    delta = degrees_to_radians(random_steering_angle)
-    yaw_rate = calculate_yaw_rate(Cy_f, Cy_r)
-    ay = v_cg * yaw_rate
-    ax = v_cg ** 2 / (lf + lr) * delta
-    assert simulate(v_cg, random_steering_angle) == (ax, ay, yaw_rate)
+    delta_torque = calculate_delta_torque(Trr, Trl)
+    Ku = ((-lf * Cy_f + lr * Cy_r) / (Izz * Vx)) - ((lf**2 * Cy_f + lr**2 * Cy_r) / (Izz * Vx)) + ((lf * Cy_f) / Izz) * St_a + (1 / 0.05 * Izz) * delta_torque
+    assert calculate_yaw_rate(Cy_f, Cy_r, Vx, St_a, Trr, Trl) == Ku

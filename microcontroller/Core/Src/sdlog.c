6c07722f0fcf6528668daa8d85b25cb1a7dd8d93
Isaak Henry <iskhnry@gmail.com>
2023-05-18 15:57:55 -0700

    SD logging functionality

--- microcontroller/Core/Src/sdlog.c
+++ microcontroller/Core/Src/sdlog.c
@@ -5,101 +5,246 @@
  *      Author: Isaak
  */
 
-/*#include "sdlog.h"
+#include "sdlog.h"
 
+//DECLARATION OF GLOBAL VARIABLES
 
+FATFS file_system;
+FIL file;
+FILINFO file_info;
+FRESULT file_result;
+UINT bytes_to_write, bytes_read;
 
-void SD_GPIO_Init(){
-	GPIO_InitTypeDef GPIO_InitStruct;
-
-	GPIO_InitStruct.Pin = SD_SPI_CS_PIN;
-	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-	GPIO_InitStruct.Pull = GPIO_PULLUP;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-	HAL_GPIO_Init(SD_SPI_GPIO_PORT, &GPIO_InitStruct);
+FRESULT sd_mount(const TCHAR * sd_path){
+	file_result = f_mount(&file_system, sd_path, 1);
+	if(file_result != FR_OK)
+		perror("!!!! ERROR !!!! SD CARD NOT MOUNTED !!!!");
+	printf("SD card mounted successfully");
+	return file_result;
 }
 
-/*void SD_SPI_Init(){
-	GPIO_InitTypeDef GPIO_InitStruct;
+FRESULT sd_unmount(const TCHAR * sd_path){
+	file_result = f_mount(NULL, sd_path, 0);
+	if(file_result == FR_OK)
+		printf("SD card has been unmounted successfully");
+	perror("!!!! ERROR !!!! SD CARD STILL MOUNTED !!!!");
+	return file_result;
+}
 
-	// Configuration of GPIO pins for SPI communication
-
-	GPIO_InitStruct.Pin =  GPIO_PIN_8 |GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11;
-	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-	GPIO_InitStruct.Pull = GPIO_NOPULL;
-	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-	GPIO_InitStruct.Alternate = GPIO_AF5_SPI1;
-	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-	// Initialization of SD Card
-
-	SD_SPI_HANDLE.Instance = SPI1;
-	SD_SPI_HANDLE.Init.Mode = SPI_MODE_MASTER;
-	SD_SPI_HANDLE.Init.Direction = SPI_DIRECTION_2LINES;
-	SD_SPI_HANDLE.Init.DataSize = SPI_DATASIZE_8BIT;
-	SD_SPI_HANDLE.Init.CLKPolarity = SPI_POLARITY_LOW;
-	SD_SPI_HANDLE.Init.CLKPhase = SPI_PHASE_1EDGE;
-	SD_SPI_HANDLE.Init.NSS = SPI_NSS_SOFT;
-	SD_SPI_HANDLE.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_256;
-	SD_SPI_HANDLE.Init.FirstBit = SPI_FIRSTBIT_MSB;
-	SD_SPI_HANDLE.Init.TIMode = SPI_TIMODE_DISABLE;
-    SD_SPI_HANDLE.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-	SD_SPI_HANDLE.Init.CRCPolynomial = 10;
-	HAL_SPI_Init(&SD_SPI_HANDLE);
-}*/
-
-/*void SD_Write(const char * filename, const char * data){
-	FATFS fs;
-	FIL file;
-	FRESULT res;
-
-	// Mount SD card
-	res = f_mount(&fs, "", 1);
-	if(res != FR_OK)
-		return;
-
-	// Open file for writing
-	res = f_open(&file, filename, FA_CREATE_ALWAYS | FA_WRITE);
-	if(res != FR_OK)
-		return;
-
-	// Write data to file
-	for(int i = 0; i < strlen(data); i++){
-		uint8_t byte = data[i];
-		res = HAL_SPI_Transmit(&SD_SPI_HANDLE, &byte, 1, HAL_MAX_DELAY);
-		if(res != HAL_OK)
-			return;
+FRESULT Write_File (char * name, char * to_write){
+	file_result = f_stat (name, &file_info);
+	//Check if file exists	``
+	if (file_result != FR_OK)
+	{
+		char * buf = malloc(100*sizeof(char));
+		sprintf (buf, "ERROR!!! *%s* does not exists\n", name);
+		printf (buf);
+	    free(buf);
+	    return file_result;
 	}
 
-	// Close file
-
-	res = f_close(&file);
-	if(res != FR_OK)
-		return;
-
-	// Unmount SD card
-	f_mount(NULL, "", 0);
-}
-
-void SD_READ_WRITE(){
-	if(HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_9))
-		printf("There is an SD card detected");
-		//HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, 1);
 	else
-		printf("No SD card detected");
-		//HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, 0);
+	{
+	    //Create a file with R/W Permissions
+	    file_result = f_open(&file, name, FA_OPEN_EXISTING | FA_WRITE);
+	    if (file_result != FR_OK)
+	    {
+	    	char * buf = malloc(100*sizeof(char));
+	    	sprintf (buf, "ERROR!!! No. %d in opening file *%s*\n", file_result, name);
+	    	printf(buf);
+	        free(buf);
+	        return file_result;
+	    }
+
+	    else
+	    {
+	    	file_result = f_write(&file, to_write, strlen(to_write), &bytes_to_write);
+	    	if (file_result != FR_OK)
+	    	{
+	    		char * buf = malloc(100*sizeof(char));
+	    		sprintf (buf, "ERROR!!! No. %d while writing to the FILE *%s*\n", file_result, name);
+	    		printf(buf);
+	    		free(buf);
+	    	}
+
+	    	//Close the file
+	    	file_result = f_close(&file);
+	    	if (file_result != FR_OK)
+	    	{
+	    		char * buf = malloc(100*sizeof(char));
+	    		sprintf (buf, "ERROR!!! No. %d in closing file *%s* after writing it\n", file_result, name);
+	    		printf(buf);
+	    		free(buf);
+	    	}
+	    	else
+	    	{
+	    		char *buf = malloc(100*sizeof(char));
+	    		sprintf (buf, "File *%s* is WRITTEN and CLOSED successfully\n", name);
+	    		printf(buf);
+	    		free(buf);
+	    	}
+	    }
+	    return file_result;
+	}
+}
+
+//ReadFile
+FRESULT sd_read(char * name){
+	/**** check whether the file exists or not ****/
+		file_result = f_stat (name, &file_info);
+		if (file_result != FR_OK)
+		{
+			char *buf = malloc(100*sizeof(char));
+			sprintf (buf, "ERRROR!!! *%s* does not exists\n", name);
+			printf (buf);
+			free(buf);
+		    return file_result;
+		}
+
+		else
+		{
+			/* Open file to read */
+			file_result = f_open(&file, name, FA_READ);
+
+			if (file_result != FR_OK)
+			{
+				char *buf = malloc(100*sizeof(char));
+				sprintf (buf, "ERROR!!! No. %d in opening file *%s*\n", file_result, name);
+				printf(buf);
+			    free(buf);
+			    return file_result;
+			}
+
+			/* Read data from the file
+			* see the function details for the arguments */
+
+			char *buffer = malloc(sizeof(f_size(&file)));
+			file_result = f_read (&file, buffer, f_size(&file), &bytes_read);
+			if (file_result != FR_OK)
+			{
+				char *buf = malloc(100*sizeof(char));
+				free(buffer);
+			 	sprintf (buf, "ERROR!!! No. %d in reading file *%s*\n", file_result, name);
+			 	printf(buffer);
+			  	free(buf);
+			}
+
+			else
+			{
+				printf(buffer);
+				free(buffer);
+
+				/* Close file */
+				file_result = f_close(&file);
+				if (file_result != FR_OK)
+				{
+					char *buf = malloc(100*sizeof(char));
+					sprintf (buf, "ERROR!!! No. %d in closing file *%s*\n", file_result, name);
+					printf(buf);
+					free(buf);
+				}
+				else
+				{
+					char *buf = malloc(100*sizeof(char));
+					sprintf (buf, "File *%s* CLOSED successfully\n", name);
+					printf(buf);
+					free(buf);
+				}
+			}
+		    return file_result;
+		}
+}
+
+//Check if SD card is saving
+void sd_check(FRESULT fresult){
+	if(fresult != FR_OK)
+		printf("!!!! ERROR !!!!! DATA IS NOT SAVING !!!!");
 }
 
 
-void test(){
-	HAL_Init();
-	SD_GPIO_Init();
-	//SD_SD_Init();
+//CreateFIle
+FRESULT file_create(char * name){
+		file_result = f_stat (name, &file_info);
+		if (file_result == FR_OK)
+		{
+			char * buf = malloc(100*sizeof(char));
+			sprintf (buf, "ERROR!!! *%s* already exists!!!!\n use Update_File \n",name);
+			printf(buf);
+			free(buf);
+		    return file_result;
+		}
+		else
+		{
+			file_result = f_open(&file, name, FA_CREATE_ALWAYS|FA_READ|FA_WRITE);
+			if (file_result != FR_OK)
+			{
+				char * buf = malloc(100*sizeof(char));
+				sprintf (buf, "ERROR!!! No. %d in creating file *%s*\n", file_result, name);
+				printf(buf);
+				free(buf);
+			    return file_result;
+			}
+			else
+			{
+				char * buf = malloc(100*sizeof(char));
+				sprintf (buf, "*%s* created successfully\n Now use Write_File to write data\n", name);
+				printf(buf);
+				free(buf);
+			}
 
-	//const char * filename = "test.txt";
-	//const char * data = "I'm working!";
-	//SD_Write(filename, data);
+			file_result = f_close(&file);
+			if (file_result != FR_OK)
+			{
+				char * buf = malloc(100*sizeof(char));
+				sprintf (buf, "ERROR No. %d in closing file *%s*\n", file_result, name);
+				printf(buf);
+				free(buf);
+			}
+			else
+			{
+				char * buf = malloc(100*sizeof(char));
+				sprintf (buf, "File *%s* CLOSED successfully\n", name);
+				printf(buf);
+				free(buf);
+			}
+		}
+	    return file_result;
+}
 
-	while(1);
-}*/
+//RemoveFile
+FRESULT file_remove(char * name){
+	/**** check whether the file exists or not ****/
+		file_result = f_stat (name, &file_info);
+		if (file_result != FR_OK){
+			char * buf = malloc(100*sizeof(char));
+			sprintf (buf, "ERROR!!! *%s* does not exists\n", name);
+			printf(buf);
+			free(buf);
+			return file_result;
+		} else{
+			file_result = f_unlink(name);
+			if (file_result == FR_OK){
+				char * buf = malloc(100*sizeof(char));
+				sprintf (buf, "*%s* has been removed successfully\n", name);
+				printf(buf);
+				free(buf);
+			}else{
+				char * buf = malloc(100*sizeof(char));
+				sprintf (buf, "ERROR No. %d in removing *%s*\n",file_result, name);
+				printf(buf);
+				free(buf);
+			}
+		}
+		return file_result;
+}
+
+
+void csv_header(char * name, int file_length){
+}
+
+
+void csv_update(char * name, int file_length){
+
+}
+
 

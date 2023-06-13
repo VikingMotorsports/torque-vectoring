662a249d5a6203b8b6c3c7340afed5126107eb73
bena24 <24benanderson@gmail.com>
2023-06-13 11:43:49 -0700

    Delete time vector creation

--- pid/src/main.rs
+++ pid/src/main.rs
@@ -11,13 +11,10 @@ fn main()
     (a_x, a_y, wheel_velocity, curr_yaw_rate, des_yaw_rate) = calc::simulate(1.0, 1.0, 1.0, 1.0, -20.0);
 
     let mut i = 1;
-    let mut time: Vec<usize> = Vec::new();
-    time.push(0);
     let mut total_slip: Vec<f32> = Vec::new();
     total_slip.push(((wheel_velocity[i] / a_x[i]) - 1.0) * 100.0);
 
     while i < a_x.len() {
-        time.push(i / 100);
         total_slip.push(((wheel_velocity[i] / a_x[i]) - 1.0) * 100.0);
         i += 1;
     }
